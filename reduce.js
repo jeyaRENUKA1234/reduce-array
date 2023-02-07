5543661211447e98ad0d4e8649a219d71a24ac8d
jeyaRENUKA1234 <renukajeyaraj24@gmail.com>
2023-02-07 12:24:57 +0530

    update

---
+++ reduce.js
@@ -0,0 +1,8 @@
+
+
+	const numbers = [1, 2, 3, 4, 5];
+			const sumReduce = numbers.reduce(
+			(accumulator, currentValue) => accumulator + currentValue,
+			0,
+			);
+			console.log("Reduce Method",sumReduce)
